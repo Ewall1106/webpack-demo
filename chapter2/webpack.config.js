574697c0af0b2a93c262bab944c2d2852748fde0
ewall <1054064180@qq.com>
2020-02-08 22:37:01 +0800

    feat：webpack从0到1-使用babel打包

--- chapter2/webpack.config.js
+++ chapter2/webpack.config.js
@@ -1,18 +1,9 @@
-const path = require("path");
-
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
-};
 // 引入node中的path模块
 const path = require("path");
 
 module.exports = {
   // 打包的模式：development/production
+  // development不会压缩代码、便于查看，也不会做一些其它性能上优化，反之production模式会做处理。
   mode: "development",
   // 定义入口文件，告诉webpack我要打包啥
   entry: "./src/index.js",
---
+++ chapter3/src/footer.js
@@ -0,0 +1,6 @@
+export function createFooter() {
+  const div = document.createElement("div");
+  div.innerText = "尾部块";
+
+  document.body.appendChild(div);
+}
